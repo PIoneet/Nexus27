86e4b9c31e4f6b5ba93f0d050b3c00699b44f88f
Sun Hong Jae <sunhongjae@gmail.com>
2026-03-30 13:49:23 +0900

    renew Repository

--- Nexus27/include/triangle_buffer.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <vector>
-
-using namespace std;
-
-void setupTriangleBuffers(vector<float>& vertices, vector<unsigned int>& indices, unsigned int& VAO, unsigned int& VBO, unsigned int& EBO);
